dc4758eb26e7f72cd0f767d7596fcd24af3de9cf
Irina <petitpapillon82@gmail.com>
2022-09-05 23:54:37 +0400

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Решение в группах задач:
+Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+
+
+Console.Clear();
+int A, B, C;
+Console.Write("Введите длины сторон треугольника: ");
+while (!int.TryParse(Console.ReadLine(), out A) || A < 0) {
+Console.Write("Ошибка! Введите положительное число: ");
+}
+Console.Write("Введите длины сторон треугольника: ");
+while (!int.TryParse(Console.ReadLine(), out B) || B < 0) {
+Console.Write("Ошибка! Введите положительное число: ");
+}
+Console.Write("Введите длины сторон треугольника: ");
+while (!int.TryParse(Console.ReadLine(), out C) || C < 0) {
+Console.Write("Ошибка! Введите положительное число: ");
+}
+
+if (A < B + C & B < A + C & C < A + B) 
+    Console.Write("Может!");
+else 
+    Console.Write("Не может!");
+Console.WriteLine( );
+
+
+Console.Clear();
+int A, B, C;
+Console.Write("Введите длины сторон треугольника: ");
+while (!int.TryParse(Console.ReadLine(), out A) || A < 0) {
+Console.Write("Ошибка! Введите положительное число: ");
+}
+Console.Write("Введите длины сторон треугольника: ");
+while (!int.TryParse(Console.ReadLine(), out B) || B < 0) {
+Console.Write("Ошибка! Введите положительное число: ");
+}
+Console.Write("Введите длины сторон треугольника: ");
+while (!int.TryParse(Console.ReadLine(), out C) || C < 0) {
+Console.Write("Ошибка! Введите положительное число: ");
+}
+
+if (A < B + C & B < A + C & C < A + B) 
+    Console.Write("Может!");
+else 
+    Console.Write("Не может!");
+Console.WriteLine( );
+Console.WriteLine( );
+
